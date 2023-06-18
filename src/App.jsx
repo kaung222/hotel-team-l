1f5041cf9c6685de0ccab38fe1cb273d06bf5a35
james marcus <thirdgodiswinning@gmail.com>
2023-06-18 18:18:35 +0630

    shop section end

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Shop from "./pages/shop/Shop";
+import Cart from "./pages/shop/Cart";
+import Wishlist from "./pages/shop/Wishlist";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
       </Routes>
     </>
   );
--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -6,13 +6,15 @@ import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
   const [showSidebar, setShowSiderbar] = useState(false);
+
   const handleSidebar = () => {
     setShowSiderbar(!showSidebar);
   };
+
   return (
     <div>
       {/* navbar  */}
-      <div className="px-5 py-3 flex items-center justify-between bg-default">
+      <div className="px-5 py-3 flex items-center z-30 justify-between bg-slate-50">
         <div className="flex items-center justify-between lg:justify-start gap-5">
           <Link to="/">
             <img
@@ -25,41 +27,83 @@ const Navbar = () => {
 
         {/* nav link  */}
 
-        <div className="flex items-center justify-center gap-7">
-          <Link className="flex items-center justify-center gap-2 hover:text-yellow-500">
-            <span>HOME</span>
-            <IoIosArrowDown />
+        <div className="hidden md:flex items-center justify-center gap-7">
+          <Link
+            className="flex items-center justify-center px-3 gap-2  group"
+            to="/home"
+          >
+            <span className="hover:text-yellow-500">HOME</span>
+            {/* <IoIosArrowDown /> */}
           </Link>
-          <Link className="flex items-center justify-center gap-2 hover:text-yellow-500">
-            <span>ROOMS</span>
-            <IoIosArrowDown />
+          <Link className="flex items-center justify-center px-3 py-2 gap-2 group 0">
+            <span className="hover:text-yellow-500">ROOMS</span>
+            <IoIosArrowDown className="group-hover:text-yellow-500" />
+            <ul className=" bg-slate-50 ml-7 hidden group-hover:block animate__fadeInUp animate__animated p-3 pt-10 duration-500 w-[200px] top-[50px] absolute ">
+              <li className="px-3 py-1 hover:ml-5 transition-all hover:text-yellow-500">
+                ROOMS
+              </li>
+              <li className=" px-3 py-1 hover:ml-5 transition-all hover:text-yellow-500">
+                ROOMS DETAILS
+              </li>
+              <li className=" px-3 py-1 hover:ml-5 transition-all hover:text-yellow-500">
+                RESTAURENT
+              </li>
+              <li className=" px-3 py-1 hover:ml-5 transition-all hover:text-yellow-500">
+                OUR CHEF
+              </li>
+            </ul>
           </Link>
-          <Link className="flex items-center justify-center gap-2 hover:text-yellow-500">
-            <span>SHOP</span>
-            <IoIosArrowDown />
+          <Link className="flex items-center justify-center px-3 group py-2 gap-2 ">
+            <span className="hover:text-yellow-500">SHOP</span>
+            <IoIosArrowDown className="group-hover:text-yellow-500" />
+            <ul className="hidden bg-slate-50 ml-7 group-hover:block animate__fadeInUp animate__animated transition-all p-3 pt-7 duration-500 w-[200px] top-[50px] absolute  ">
+              <Link to="/shop">
+                <li className=" px-3 py-1 hover:ml-5 transition-all hover:text-yellow-500">
+                  SHOP
+                </li>
+              </Link>
+              <Link to="/cart">
+                <li className=" px-3 py-1 hover:ml-5 transition-all hover:text-yellow-500">
+                  CART
+                </li>
+              </Link>
+              <Link to="/wishlist">
+                <li className=" px-3 py-1 hover:ml-5 transition-all hover:text-yellow-500">
+                  WISHLIST
+                </li>
+              </Link>
+            </ul>
           </Link>
-          <Link className="flex items-center justify-center gap-2 hover:text-yellow-500">
-            <span>BLOG</span>
-            <IoIosArrowDown />
+          <Link className="flex items-center justify-center px-3 py-2 gap-2 hover:text-yellow-500">
+            <span>CONTACT</span>
           </Link>
         </div>
 
         {/* nav link end  */}
         <div className="flex items-center justify-center gap-5">
-          <button className="items-center hover:bg-black hover:text-white hidden lg:flex justify-center uppercase w-[250px] gap-3 bg-white py-3 px-5">
+          <button className="items-center hover:bg-black transition-all duration-300 hover:text-white hidden lg:flex justify-center uppercase w-[250px] gap-3 btn py-3 px-5">
             <span className="text-xl">Book Now</span>
             <BsArrowUpRight />
           </button>
 
-          <button className="" onClick={(e) => handleSidebar(e)}>
-            <p className="w-16 h-1 border-b-2 border-slate-700 bg-slate-500"></p>
-            <p className="w-12 h-1 border-b-2 my-2 border-slate-700 bg-slate-500 hover:w-16 duration-300"></p>
-            <p className="w-16 h-1 border-b-2 border-slate-700 bg-slate-500"></p>
+          <button className="group" onClick={(e) => handleSidebar(e)}>
+            <p className="w-16 h-1 border-b-[1px] border-slate-500 group-hover:border-yellow-500 bg-slate-500"></p>
+            <p className="w-12 h-1 border-b-[1px] my-2 border-slate-500 group-hover:border-yellow-500 bg-slate-500 group-hover:w-16 duration-300"></p>
+            <p className="w-16 h-1 border-b-[1px] border-slate-500 group-hover:border-yellow-500 bg-slate-500"></p>
           </button>
         </div>
       </div>
-      {/* hidden sidebar  */}
-      {showSidebar && <Sidebar setShowSidebar={setShowSiderbar} />}
+      {showSidebar && (
+        <div className=" w-screen h-screen z-50 fixed top-0 ">
+          <div
+            className="  w-full h-full blur-md"
+            onClick={() => setShowSiderbar(false)}
+          ></div>
+          {/* hidden sidebar  */}
+
+          <Sidebar setShowSidebar={setShowSiderbar} />
+        </div>
+      )}
     </div>
   );
 };
