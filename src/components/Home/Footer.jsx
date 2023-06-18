39f46d3a242aa71359775641e3f5eed5349f610e
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2023-06-18 20:20:52 +0630

    footer responsive

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <div className=" grid grid-cols-10 gap-10 px-5 py-20 bg-gray-800 h-full border-b border-slate-500">
-        <div className=" flex flex-col col-span-3 gap-10 ">
-          <h4 className=" text-white text-3xl font-serif">About Us</h4>
+      <div className=" grid md:grid-cols-10 gap-10 px-5 py-5 md:py-20 bg-gray-800 h-full border-b border-slate-500">
+        <div className=" flex flex-col col-span-3 gap-5 md:gap-10 ">
+          <h4 className=" text-white text-lg md:text-3xl font-serif">
+            About Us
+          </h4>
           <p className=" text-white text-sm leading-6">
             Welcome to our Royel Hotel. We are provided luxury room for you. It
             was popularised in the 1960s with the release of Letraset sheets
@@ -27,9 +29,11 @@ const Footer = () => {
             </button>
           </div>
         </div>
-        <div className=" flex flex-col col-span-2 gap-10">
-          <h4 className=" text-white text-3xl font-serif">Useful Link</h4>
-          <div className=" flex flex-col gap-2">
+        <div className=" flex flex-col col-span-2 gap-5 md:gap-10">
+          <h4 className=" text-white text-lg md:text-3xl font-serif">
+            Useful Link
+          </h4>
+          <div className=" flex flex-wrap md:flex-col gap-5 md:gap-2">
             <p className=" text-white font-serif">About Us</p>
             <Link to={"/room"}>
               <p className=" text-white font-serif">Rooms</p>
@@ -42,8 +46,10 @@ const Footer = () => {
             <p className=" text-white font-serif">Get In Touch</p>
           </div>
         </div>
-        <div className=" flex flex-col col-span-2 gap-10 ">
-          <h4 className=" text-white text-3xl font-serif">Contact Us</h4>
+        <div className=" flex flex-col col-span-2 gap-5 md:gap-10 ">
+          <h4 className=" text-white text-lg md:text-3xl font-serif">
+            Contact Us
+          </h4>
           <div className=" flex flex-col gap-3">
             <p className=" text-white font-serif">
               Via Giovanni Augustariccio, 33 Amalfi, 84011 SA, Dubai, UAE
@@ -52,8 +58,10 @@ const Footer = () => {
             <p className=" text-white font-serif">info@theroyelpalace.com</p>
           </div>
         </div>
-        <div className="flex flex-col col-span-3 gap-10">
-          <h4 className=" text-white text-3xl font-serif">Newsletter</h4>
+        <div className="flex flex-col col-span-3 gap-5 md:gap-10">
+          <h4 className="text-white text-lg md:text-3xl font-serif">
+            Newsletter
+          </h4>
           <p className=" text-white font-serif">
             Subscribe us to get our hotel offer, latest update & news.
           </p>
@@ -61,10 +69,10 @@ const Footer = () => {
           <div className=" input-group">
             <input
               type="email"
-              className=" px-4 py-4 border border-slate-500 bg-transparent text-white"
+              className=" px-4 py-2 md:py-4 border border-slate-500 bg-transparent text-white"
               placeholder="Your Email"
             />
-            <button className="px-4 py-4 border border-orange-200 bg-orange-200">
+            <button className="px-4 md:py-4 border border-orange-200 bg-orange-200">
               <BsArrowUpRight />
             </button>
           </div>
@@ -72,7 +80,7 @@ const Footer = () => {
       </div>
       <div className=" px-5 py-4 bg-gray-800 h-full">
         <p className=" text-white text-center font-serif">
-          &copy; 2023 Royel All Rights Reserved
+          &copy; 2023 Team-L Workshop All Rights Reserved
         </p>
       </div>
     </>
